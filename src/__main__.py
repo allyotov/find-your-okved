0602f63beb5b95b6aaedbdf060330d55669af4b8
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-09 19:55:48 +0300

    feat/переработал точку входа

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,4 @@
-import logging
+from src.commands.cli import app
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
-
-
-def main():
-    logger.info('Утилита "Найди свой ОКВЭД по номеру телефона" запущена.')
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app()
