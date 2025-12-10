7900c1b6592f657039473ac161ec1955f1ae8534
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-10 10:42:12 +0300

    feat/загрузка настрек cache репозитория

--- src/settings.py
+++ src/settings.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from pyhocon import ConfigFactory
 
 github_config = ConfigFactory.parse_file('settings/github.conf')
+cache_config = ConfigFactory.parse_file('settings/cache.conf')
 
 
 class GithubSettings(BaseModel):
@@ -10,4 +11,10 @@ class GithubSettings(BaseModel):
     file_path: str = github_config['config']['file_path']
 
 
+class CacheSettings(BaseModel):
+    etag_cache_path: str = cache_config['config']['etag_cache_path']
+    okved_json_cache_path: str = cache_config['config']['okved_json_cache_path']
+
+
 GITHUB_SETTINGS = GithubSettings()
+CACHE_SETTINGS = CacheSettings()
