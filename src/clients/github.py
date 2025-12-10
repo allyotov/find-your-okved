83c33ebbb6440bb8e88d1d6498a3421a815f63f7
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-10 11:56:05 +0300

    feat/методы GithubClient

--- src/clients/github.py
+++ src/clients/github.py
@@ -1,6 +1,8 @@
+import base64
+import json
 import logging
 
-from httpx import Client
+from httpx import Client as httpx_client
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -11,25 +13,50 @@ OKVED_JSON_GITHUB_URL_TEMPLATE = 'https://api.github.com/repos/{owner}/{repo}/co
 
 class GithubClient:
     def __init__(self, owner: str, repo: str, file_path: str) -> None:
-        self._http_client = Client()
         self._owner: str = owner
         self._repo: str = repo
         self._file_path: str = file_path
 
-    def get_github_file_metadata(
-        self,
-    ) -> dict | None:
+    def check_okved_json_etag(self, cached_etag: str | None = None) -> dict | None:
         url = OKVED_JSON_GITHUB_URL_TEMPLATE.format(owner=self._owner, repo=self._repo, file_path=self._file_path)
 
         headers = {'Accept': 'application/vnd.github.v3+json'}
 
-        # if token:
-        #     headers['Authorization'] = f'token {token}'
+        if cached_etag:
+            headers['If-None-Match'] = cached_etag
 
-        with self._http_client as client:
+        with httpx_client() as client:
+            # TODO: добавить обработку исключений запроса;
             response = client.head(url, headers=headers, follow_redirects=True)
 
-            if response.status_code == 200:
-                return response.headers.get('etag')
-            else:
-                pass
+        if response.status_code == 304:
+            logger.info("etag didn't change.")
+        elif response.status_code == 200:
+            logger.info('etag updated.')
+            new_etag = response.headers.get('ETag')
+            if new_etag:
+                return new_etag
+        else:
+            logger.warning('Check github okved.json etag failed: %s - %s', response.status_code, response.text)
+
+        return None
+
+    def load_okved_json(self) -> list | None:
+        url = OKVED_JSON_GITHUB_URL_TEMPLATE.format(owner=self._owner, repo=self._repo, file_path=self._file_path)
+
+        headers = {'Accept': 'application/vnd.github.v3+json'}
+
+        with httpx_client() as client:
+            # TODO: добавить обработку исключений запроса;
+            response = client.get(url, headers=headers)
+
+        okved_codes = None
+
+        if response.status_code == 200:
+            content = response.json()['content']
+            decoded = base64.b64decode(content).decode('utf-8')
+            okved_codes = json.loads(decoded)
+        else:
+            logger.warning('Loading github okved.json failed: %s - %s', response.status_code, response.text)
+
+        return okved_codes
