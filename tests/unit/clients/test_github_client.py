e74cdc033afc91d6256190af3ae3437d294e98fa
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-10 22:47:32 +0300

    feat/использую mock .get() и head() методов httpx.Client()

--- tests/unit/clients/test_github_client.py
+++ tests/unit/clients/test_github_client.py
@@ -1,29 +1,54 @@
+import base64
+import json
+from unittest.mock import patch
+
+import httpx
 import pytest
 
 from src.clients.github import GithubClient
-from src.settings import GITHUB_SETTINGS
 
-# TODO: необходимо написать фикстуру, которая будет возвращать mock httpx клиента github API, поскольку мы не должны
-# проверять в своих модульных тестах работу стороннего API;
+
+@pytest.fixture
+def mock_httpx_client_head():
+    with patch('src.clients.github.httpx_client.head') as mock:
+        yield mock
+
+
+@pytest.fixture
+def mock_httpx_client_get():
+    with patch('src.clients.github.httpx_client.get') as mock:
+        yield mock
 
 
 @pytest.fixture
 def github_client() -> GithubClient:
-    return GithubClient(owner=GITHUB_SETTINGS.owner, repo=GITHUB_SETTINGS.repo, file_path=GITHUB_SETTINGS.file_path)
+    return GithubClient(owner='test_owner', repo='test_case_repo', file_path='okveds.json')
 
 
-def test_get_new_okved_json_etag(github_client):
+def test_get_new_okved_json_etag(github_client, mock_httpx_client_head):
+    mock_httpx_client_head.return_value = httpx.Response(200, headers={'ETag': 'test_etag_string'})
     etag = github_client.check_okved_json_etag(cached_etag=None)
     assert etag
-    assert type(etag) is str
+    assert etag == 'test_etag_string'
 
 
-def test_check_actual_okved_json_etag(github_client):
-    cached_etag = github_client.check_okved_json_etag(cached_etag=None)
+def test_check_actual_okved_json_etag(github_client, mock_httpx_client_head):
+    mock_httpx_client_head.return_value = httpx.Response(304, headers={})
+    cached_etag = 'test_etag_string'
     check_result = github_client.check_okved_json_etag(cached_etag=cached_etag)
     assert check_result is None
 
 
-def test_get_new_okved_json(github_client):
+def test_get_new_okved_json(github_client, mock_httpx_client_get):
+    okveds_list = [{'code': 123, 'name': 'abc', 'items': []}]
+    okveds_encoded_bytes = _get_encoded_bytes_from_list(okveds_list)
+    mock_httpx_client_get.return_value = httpx.Response(200, json={'content': okveds_encoded_bytes})
     okved_codes = github_client.load_okved_json()
-    assert type(okved_codes) is list
+
+    assert okved_codes == okveds_list
+
+
+def _get_encoded_bytes_from_list(given_list: list[dict]) -> str:
+    okveds_json_string = json.dumps(given_list)
+    bytes_string = okveds_json_string.encode('utf-8')
+    return base64.b64encode(bytes_string).decode('ascii')
