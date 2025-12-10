0a95a9a8aadb616be513a7867a00248debe7437f
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-10 10:44:50 +0300

    feat/тесты загрузки etag из файла

---
+++ tests/integrational/test_cache_repo.py
@@ -0,0 +1,75 @@
+import json
+from pathlib import Path
+
+import pytest
+
+from src.repositories.cache import CacheRepository
+
+TEST_ETAG_CACHE_PATH = 'test_etag_cache.json'
+TEST_OKVED_JSON_PATH = 'test_okved.json'
+TEST_CACHED_ETAG = 'test_etag_str'
+
+
+@pytest.fixture()
+def cache_repo():
+    return CacheRepository(etag_cache_path=TEST_ETAG_CACHE_PATH, okved_json_cache_path=TEST_OKVED_JSON_PATH)
+
+
+@pytest.fixture
+def correct_etag_json_file():
+    file_path = Path(TEST_ETAG_CACHE_PATH)
+
+    data = {'etag': TEST_CACHED_ETAG}
+    with file_path.open('w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+    yield file_path
+
+    if file_path.exists():
+        file_path.unlink()
+
+
+@pytest.fixture
+def invalid_etag_json_file():
+    file_path = Path(TEST_ETAG_CACHE_PATH)
+
+    # Записываем заведомо некорректный JSON
+    invalid_content = """{
+        "etag": "test_etag"
+    """  # Отсутствует закрывающая скобка
+
+    with file_path.open('w', encoding='utf-8') as f:
+        f.write(invalid_content)
+
+    yield file_path
+
+    if file_path.exists():
+        file_path.unlink()
+
+
+@pytest.fixture
+def wrong_key_etag_json_file():
+    file_path = Path(TEST_ETAG_CACHE_PATH)
+
+    data = {'_e_tag_': TEST_CACHED_ETAG}
+    with file_path.open('w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+    yield file_path
+
+    if file_path.exists():
+        file_path.unlink()
+
+
+def test_load_etag__file_exists__returns_etag(cache_repo, correct_etag_json_file):
+    assert cache_repo.get_okved_json_etag_from_cache() == TEST_CACHED_ETAG
+
+
+def test_load_etag__file_doesnt_exists__returns_none(cache_repo):
+    assert cache_repo.get_okved_json_etag_from_cache() is None
+
+
+def test_load_etag__invalid_file__returns_none(cache_repo, invalid_etag_json_file):
+    assert cache_repo.get_okved_json_etag_from_cache() is None
+
+
+def test_load_etag__no_etag_key__returns_none(cache_repo, wrong_key_etag_json_file):
+    assert cache_repo.get_okved_json_etag_from_cache() is None
