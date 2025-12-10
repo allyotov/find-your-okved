cbb38de411246ab08fa34745f15e69d7873ca55f
Alexander Lyotov <alexander.lyotov@yandex.ru>
2025-12-10 11:56:20 +0300

    feat/тесты методов GithubClient

--- tests/integrational/test_github_client.py
+++ tests/integrational/test_github_client.py
@@ -9,6 +9,18 @@ def github_client() -> GithubClient:
     return GithubClient(owner=GITHUB_SETTINGS.owner, repo=GITHUB_SETTINGS.repo, file_path=GITHUB_SETTINGS.file_path)
 
 
-def test_get_okved_json_metadata__gets_etag_as_str(github_client):
-    date_str = github_client.get_github_file_metadata()
-    assert type(date_str) is str
+def test_get_new_okved_json_etag(github_client):
+    etag = github_client.check_okved_json_etag(cached_etag=None)
+    assert etag
+    assert type(etag) is str
+
+
+def test_check_actual_okved_json_etag(github_client):
+    cached_etag = github_client.check_okved_json_etag(cached_etag=None)
+    check_result = github_client.check_okved_json_etag(cached_etag=cached_etag)
+    assert check_result is None
+
+
+def test_get_new_okved_json(github_client):
+    okved_codes = github_client.load_okved_json()
+    assert type(okved_codes) is list
